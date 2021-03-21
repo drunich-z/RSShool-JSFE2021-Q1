702ab09c5b55ee74ed39c6d00eeafaff872d29de
drunich-z <zverev-and@yandex.ru>
2021-03-21 18:10:31 +0700

    feat: add play sounds

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -4,10 +4,17 @@ const bodyPage = document.getElementsByTagName("body")[0];
 const btnNotes = document.querySelector(".btn-notes");
 const btnLetters = document.querySelector(".btn-letters");
 
-//const BTN = document.getElementById("btn.container");
+function playAudio(src) {
+  const audio = new Audio();
+  audio.src = src;
+  audio.currentTime = 0;
+  audio.play();
+}
 
 const startSound = (event) => {
   event.target.classList.add("piano-key-active");
+  const note = event.target.dataset.note;
+  playAudio(`./assets/audio/${note}.mp3`);
 };
 
 const stopSound = (event) => {
@@ -17,6 +24,8 @@ const stopSound = (event) => {
 const startCorrespondOver = (event) => {
   if (event.target.classList.contains("piano-key")) {
     event.target.classList.add("piano-key-active");
+    const note = event.target.dataset.note;
+    playAudio(`./assets/audio/${note}.mp3`);
   }
   pianoKeys.forEach((elem) => {
     elem.addEventListener("mouseover", startSound);
@@ -37,8 +46,6 @@ const changePianoView = (event) => {
   if (event.target.classList.contains("btn-notes")) {
     event.target.classList.add("btn-active");
     btnLetters.classList.remove("btn-active");
-    pianoKeys[0].classList.remove("piano-key-letter");
-    pianoKeys[0].classList.add("letter");
     pianoKeys.forEach((elem) => {
       elem.classList.remove("letter");
     });
@@ -47,8 +54,6 @@ const changePianoView = (event) => {
   event.target.classList.add("btn-active");
   btnNotes.classList.remove("btn-active");
   pianoKeys.forEach((elem) => elem.classList.add("letter"));
-  pianoKeys[0].classList.remove("letter");
-  pianoKeys[0].classList.add("piano-key-letter");
 };
 
 piano.addEventListener("mousedown", startCorrespondOver, false);
