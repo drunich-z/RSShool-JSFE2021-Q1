8da77cfbea5ec1a69595bbaff3916a31794a7ce5
drunich-z <zverev-and@yandex.ru>
2021-03-22 00:29:39 +0700

    feat: add fullscreen mode, change keyboard play

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -3,6 +3,21 @@ const piano = document.querySelector(".piano");
 const bodyPage = document.getElementsByTagName("body")[0];
 const btnNotes = document.querySelector(".btn-notes");
 const btnLetters = document.querySelector(".btn-letters");
+const btnFullScreen = document.querySelector(".fullscreen");
+const keyboardCodes = {
+  68: "D",
+  82: "R",
+  70: "F",
+  84: "T",
+  71: "G",
+  72: "H",
+  85: "U",
+  74: "J",
+  73: "I",
+  75: "K",
+  79: "O",
+  76: "L",
+};
 
 function playAudio(src) {
   const audio = new Audio();
@@ -66,7 +81,7 @@ const startPlayKey = (event) => {
   if (event.repeat) return;
   if (
     (pressedKey = piano.querySelector(
-      `.piano-key[data-letter=${event.key.toUpperCase()}]`
+      `.piano-key[data-letter=${keyboardCodes[event.keyCode]}]`
     ))
   ) {
     playAudio(`./assets/audio/${pressedKey.dataset.note}.mp3`);
@@ -76,7 +91,7 @@ const startPlayKey = (event) => {
 const stopPlayKey = (event) => {
   if (
     (pressedKey = piano.querySelector(
-      `.piano-key[data-letter=${event.key.toUpperCase()}]`
+      `.piano-key[data-letter=${keyboardCodes[event.keyCode]}]`
     ))
   ) {
     pressedKey.classList.remove("piano-key-active");
@@ -85,3 +100,40 @@ const stopPlayKey = (event) => {
 
 window.addEventListener("keydown", startPlayKey);
 window.addEventListener("keyup", stopPlayKey);
+/*
+const changeFullScreenMode = ()
+
+keyFullScreen.addEventListener("click", changeFullScreenMode);
+/*document
+  .querySelector(".openfullscreen")
+  .addEventListener("click", toggleScreen);*/
+
+// Открытие приложения в fullscreen
+//const fullscreenBtn = document.querySelector(".openfullscreen");
+
+function activateFullscreen(element) {
+  if (element.requestFullscreen) {
+    element.requestFullscreen(); // W3C spec
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen(); // Firefox
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen(); // Safari
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen(); // IE/Edge
+  }
+}
+function deactivateFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
+
+btnFullScreen.addEventListener("click", () => {
+  document.fullscreen
+    ? deactivateFullscreen()
+    : activateFullscreen(document.documentElement);
+});
