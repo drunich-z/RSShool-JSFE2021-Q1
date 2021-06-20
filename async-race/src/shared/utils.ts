28f95ce0801553b8b01bc9afe224c369b3a6e252
drunich-z <zverev-and@yandex.ru>
2021-06-20 22:08:28 +0700

    fix: const naming, magic numbers

--- async-race/src/shared/utils.ts
+++ async-race/src/shared/utils.ts
@@ -21,8 +21,11 @@ function getRandomName(): string {
 
 function getRandomColor(): string {
   const letters = '0123456789ABCDEF';
+  const colorStringLength = 6;
+  const cardinalityOfLettersSet = 16;
+
   let color = '#';
-  for (let i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
+  for (let i = 0; i < colorStringLength; i++) color += letters[Math.floor(Math.random() * cardinalityOfLettersSet)];
   return color;
 }
 
