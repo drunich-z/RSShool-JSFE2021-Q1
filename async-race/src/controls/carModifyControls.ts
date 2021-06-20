520af21ef5193456e17415924518106e60b84610
drunich-z <zverev-and@yandex.ru>
2021-06-20 22:57:21 +0700

    fix: refactor variables init

--- async-race/src/controls/carModifyControls.ts
+++ async-race/src/controls/carModifyControls.ts
@@ -5,41 +5,41 @@ import Store from '../shared/store';
 const DEFAULT_CAR_COLOR = '#ffffff';
 
 export default {
-  inputUpdateName: document.getElementById('update-name'),
-  inputUpdateColor: document.getElementById('update-color'),
-  submitUpdateButton: document.getElementById('update-submit'),
-  submitUpdate: document.getElementById('update'),
+  inputUpdateName: document.getElementById('update-name') as HTMLInputElement,
+  inputUpdateColor: document.getElementById('update-color') as HTMLInputElement,
+  submitUpdateButton: document.getElementById('update-submit') as HTMLButtonElement,
+  submitUpdate: document.getElementById('update') as HTMLFormElement,
 
-  inputCreateName: document.getElementById('create-name'),
-  inputCreateColor: document.getElementById('create-color'),
-  submitCreateButton: document.getElementById('create-submit'),
-  submitCreate: document.getElementById('create'),
+  inputCreateName: document.getElementById('create-name') as HTMLInputElement,
+  inputCreateColor: document.getElementById('create-color') as HTMLInputElement,
+  submitCreateButton: document.getElementById('create-submit') as HTMLButtonElement,
+  submitCreate: document.getElementById('create') as HTMLFormElement,
 
   init(): void {
-    this.inputUpdateName = document.getElementById('update-name');
-    this.inputUpdateColor = document.getElementById('update-color');
-    this.submitUpdateButton = document.getElementById('update-submit');
-    this.submitUpdate = document.getElementById('update');
+    this.inputUpdateName = document.getElementById('update-name') as HTMLInputElement;
+    this.inputUpdateColor = document.getElementById('update-color') as HTMLInputElement;
+    this.submitUpdateButton = document.getElementById('update-submit') as HTMLButtonElement;
+    this.submitUpdate = document.getElementById('update') as HTMLFormElement;
 
-    this.inputCreateName = document.getElementById('create-name');
-    this.inputCreateColor = document.getElementById('create-color');
-    this.submitCreateButton = document.getElementById('create-submit');
-    this.submitCreate = document.getElementById('create');
+    this.inputCreateName = document.getElementById('create-name') as HTMLInputElement;
+    this.inputCreateColor = document.getElementById('create-color') as HTMLInputElement;
+    this.submitCreateButton = document.getElementById('create-submit') as HTMLButtonElement;
+    this.submitCreate = document.getElementById('create') as HTMLFormElement;
   },
 
   async listenCreateCar(): Promise<void> {
     (this.submitCreate as HTMLElement).addEventListener('submit', async (event) => {
       event.preventDefault();
       const car: CarCreate = {
-        name: (this.inputCreateName as HTMLInputElement).value,
-        color: (this.inputCreateColor as HTMLInputElement).value,
+        name: this.inputCreateName.value,
+        color: this.inputCreateColor.value,
       };
       await Model.createCar(car);
       await Store.updateStoreGarage();
       PageControls.updateNextPrevButtonsState('garage');
       PageControls.updateGarageView();
-      (this.inputCreateName as HTMLInputElement).value = '';
-      (this.inputCreateColor as HTMLInputElement).value = DEFAULT_CAR_COLOR;
+      this.inputCreateName.value = '';
+      this.inputCreateColor.value = DEFAULT_CAR_COLOR;
     });
   },
 
@@ -48,24 +48,24 @@ export default {
       event.preventDefault();
       if (!Store.selectedCar.id) return;
       const updateBody = {
-        name: (this.inputUpdateName as HTMLInputElement).value,
-        color: (this.inputUpdateColor as HTMLInputElement).value,
+        name: this.inputUpdateName.value,
+        color: this.inputUpdateColor.value,
       };
       await Model.updateCar(Store.selectedCar.id, updateBody);
       await Store.updateStoreGarage();
       PageControls.updateNextPrevButtonsState('garage');
       PageControls.updateGarageView();
-      (this.inputUpdateName as HTMLInputElement).value = '';
-      (this.inputUpdateColor as HTMLInputElement).value = DEFAULT_CAR_COLOR;
+      this.inputUpdateName.value = '';
+      this.inputUpdateColor.value = DEFAULT_CAR_COLOR;
       Store.selectedCar = { name: '', color: '', id: 0 };
       this.setUpdateControlsEnable(false);
     });
   },
 
   setUpdateControlsEnable(value: boolean): void {
-    (this.inputUpdateName as HTMLInputElement).disabled = !value;
-    (this.inputUpdateColor as HTMLInputElement).disabled = !value;
-    (this.submitUpdateButton as HTMLButtonElement).disabled = !value;
+    this.inputUpdateName.disabled = !value;
+    this.inputUpdateColor.disabled = !value;
+    this.submitUpdateButton.disabled = !value;
   },
 
 };
--- async-race/src/shared/store.ts
+++ async-race/src/shared/store.ts
@@ -7,17 +7,12 @@ const { items: winners, count: winnersCount } = await Model.getWinners({
   page: 1, limit: 10, sort: 'id', order: 'ASC',
 });
 
-// const animation: any = {};
 const animation: Record<number, AnimationId> = {};
 const selectedCar: Car = { name: '', color: '', id: 0 };
 const view = 'garage' as PageView;
 const sortBy = 'id' as SortType;
 const sortOrder = 'desc' as OrderType;
 
-// не разобрался как сказать что свойства в export defaults определенного типа (самоопределнного), чтобы линт не ругался
-// let sortBy: Sort; sortBy = 'wins'; sortBy = 'id';
-// let view: PageView; view = 'winners'; view = 'garage';
-// let sortOrder: Order; sortOrder = 'DESC'; sortOrder = 'ASC';
 const PAGE_WINNERS_ITEMS_LIMIT = 10;
 const PAGE_GARAGE_ITEMS_LIMIT = 7;
 
@@ -37,7 +32,6 @@ export default {
   selectedCar,
 
   async updateStoreGarage(): Promise<void> {
-    //   const { items, count } = await Model.getCars(this.carsPage);
     const { items, count } = await Model.getCarsPlusFlag(this.carsPage);
     this.cars = items;
     this.carsCount = count;
@@ -55,9 +49,6 @@ export default {
   },
 
   setForceStopFlag(id: number, value:boolean): void {
-    // Вероника, как с потенциальным undefind бороться?
-    // У меня эти вставки кода ужасные такие
-    // в этом месте флаг принудительной остановки сбрасываю.
     const currentCarInStore = this.cars.find((carr) => carr.id === id);
     if (currentCarInStore) currentCarInStore.forceStop = value;
   },
